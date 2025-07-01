e8387eb9d8fa3561f605df167209b49f0027047c
MilaMi94 <mila.bogdanovic94@gmail.com>
2025-07-01 13:17:38 +0200

    changed docker and docker compose files for dev development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,   // <--- this is important for WSL2 or Docker
+    },
+    host: true,
+    port: 3000,
+  },
 })
